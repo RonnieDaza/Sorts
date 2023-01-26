5a7fe09d1a3c5cb7e1df134f4af3f7864b622fc8
Ronnie M. Daza Jr <daza.ronnie.09212002@gmail.com>
2023-01-26 12:40:25 +0800

    Print unsorted array

--- Selection Sort.py
+++ Selection Sort.py
@@ -9,3 +9,7 @@ def selectionSort(array, size):
                 min_idx = i
          
         (array[step], array[min_idx]) = (array[min_idx], array[step])
+
+array = [37, 88, 63, 39, 68, 1, 85, 49, 33, 21]
+print('The unsorted array is: ')
+print(array)
