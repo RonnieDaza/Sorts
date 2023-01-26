dc9d7d42faa8c529b1db94a9b977c4c37e25c48a
Ronnie M. Daza Jr <daza.ronnie.09212002@gmail.com>
2023-01-26 12:40:56 +0800

    Print sorted array in ascending order

--- Selection Sort.py
+++ Selection Sort.py
@@ -10,6 +10,12 @@ def selectionSort(array, size):
          
         (array[step], array[min_idx]) = (array[min_idx], array[step])
 
+
 array = [37, 88, 63, 39, 68, 1, 85, 49, 33, 21]
 print('The unsorted array is: ')
+print(array)
+
+size = len(array)
+selectionSort(array, size)
+print('The sorted array in ascending order is:')
 print(array)
