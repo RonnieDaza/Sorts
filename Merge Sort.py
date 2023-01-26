740658075578e5ba527bafe94e8716ff88b76d24
Ronnie M. Daza Jr <daza.ronnie.09212002@gmail.com>
2023-01-26 12:49:16 +0800

    Create while loops

--- Merge Sort.py
+++ Merge Sort.py
@@ -7,3 +7,24 @@ def mergeSort(array):
 
         mergeSort(L)
         mergeSort(M)
+        
+        i = j = k = 0
+
+        while i < len(L) and j < len(M):
+            if L[i] < M[j]:
+                array[k] = L[i]
+                i += 1
+            else:
+                array[k] = M[j]
+                j += 1
+            k += 1
+
+        while i < len(L):
+            array[k] = L[i]
+            i += 1
+            k += 1
+
+        while j < len(M):
+            array[k] = M[j]
+            j += 1
+            k += 1
